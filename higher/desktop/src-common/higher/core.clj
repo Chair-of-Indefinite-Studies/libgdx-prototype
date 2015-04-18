b1639638d87f83d4746a0fe51aa74d6d66d2ac22
dvberkel <daan.v.berkel.1980@gmail.com>
2015-04-18 10:11:18 +0200

    Accelerate entities

--- higher/desktop/src-common/higher/core.clj
+++ higher/desktop/src-common/higher/core.clj
@@ -2,6 +2,16 @@
   (:require [play-clj.core :refer :all]
             [play-clj.g2d :refer :all]))
 
+(defn accelerate-horizontally [entity direction]
+  "change entity vx depending on direction"
+  (case direction
+    :left (assoc entity :vx (dec (:vx entity)))
+    :right (assoc entity :vx (inc (:vx entity)))))
+
+(defn move [entity]
+  "Move entity according to current velocity"
+  (assoc entity :x (+ (:x entity) (:vx entity)) :y (+ (:y entity) (:vy entity))))
+
 (defscreen main-screen
   :on-show
   (fn [screen entities]
@@ -11,7 +21,8 @@
   :on-render
   (fn [screen entities]
     (clear!)
-    (render! screen entities))
+    (render! screen entities)
+    (map move entities))
 
   :on-key-down
   (fn [screen entities]
@@ -21,9 +32,9 @@
       (= (:key screen) (key-code :dpad-down))
       (println "down")
       (= (:key screen) (key-code :dpad-left))
-      (println "left")
+      (map #(accelerate-horizontally % :left) entities)
       (= (:key screen) (key-code :dpad-right))
-      (println "right"))))
+      (map #(accelerate-horizontally % :right) entities))))
 
 (defgame higher-game
   :on-create
